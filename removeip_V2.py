d9c32df4277e3ddbcc12c89d70d4180e5f8cb09a
DioLin <ccueeturkey@gmail.com>
2018-10-03 17:17:51 +0800

    Add files via upload

--- removeip_V2.py
+++ removeip_V2.py
@@ -3,7 +3,7 @@
 
 # In[1]:
 
-
+import argparse
 import pandas as pd
 from cyberpandas import IPArray
 from cyberpandas import ip_range
@@ -12,7 +12,7 @@ from cyberpandas import ip_range
 
 # In[6]:
 
-
+'''
 #initial parameters 
 FilePath='./M3 raw-data/Q3/CHT/M3_part15/2/CSV/en/vulnerabilities.csv'
 FilePathCt='./M3 raw-data/Q3/CHT/M3_part15/2/CSV/ct/vulnerabilities.csv'
@@ -28,7 +28,7 @@ print('=========================')
 print('The first 5 En\'s data are\n',df.head())
 print('=========================')
 print('The first 5 Ct\'s data are\n',dfCt.head())
-
+'''
 
 # In[15]:
 
@@ -54,19 +54,34 @@ def RemoveIP(raw_df,IP_columns_position,StartIP,EndIP):
 
 # main function
 if __name__ == '__main__':
+    print('''
 
-    parser = argparse.ArgumentParser(description='Remove IP or IP range you want.')
-    parser.add_argument("-s", "--start-ip", metavar="", help="remove form IP", default="127.0.0.1")
-    parser.add_argument("-e", "--end-ip", metavar="", help="remove  IP", default="127.0.0.1")
-    parser.add_argument("-f", "--file-read", metavar="", help="remove form IP", default="127.0.0.1")
-new_df=RemoveIP(df,0,start_ip,end_ip)
-new_dfCt=RemoveIP(dfCt,0,startIP,endIP)
-print('before and after rows(en)=',df.shape[0],new_df.shape[0])
-print('before and after rows(ct)=',dfCt.shape[0],new_dfCt.shape[0])
+        | |  | \ | |     | | | | | \ / |  | \ | |   | | | |        | |  | |  | \ 
+        | |__| | | |---- | | | | | | | |  | | \ \   / / | |----    | |  | |__|_/ 
+        |_|  \_\ |_|____ |_| |_| |_| \_|__|_/  \_\_/_/  |_|____   _|_|_ |_|      
+
+        Author:Dio
+
+        example: (If you only need to keep 10.1.1.0/24,and column 0 is IP's filed)
+
+        python removeip_V2.py -s 10.1.1.1 -e 10.1.1.255 -f 0 -r './M3 raw-data/Q3/CHT/M3_part15/2/CSV/en/vulnerabilities.csv' -w 'vulnerabilities_en.csv'
+        ''')
+    parser = argparse.ArgumentParser(description='leave IP or IP range you want to keep.')
+    parser.add_argument("-s", "--start-ip", metavar="", help="start form IP", default="127.0.0.1")
+    parser.add_argument("-e", "--end-ip", metavar="", help="end of IP", default="127.0.0.1")
+    parser.add_argument("-f", "--specific-field", metavar="", help="which field include IP",type=int, default="0")
+    parser.add_argument("-r", "--file-read", metavar="", help="the file you want to remove/keep certain IP", default="")
+    parser.add_argument("-w", "--file-write", metavar="", help="save path", default="")
+    args = parser.parse_args()
+
+    df = pd.read_csv(args.file_read)
+    #print(df.tail())
+    new_df=RemoveIP(df,args.specific_field,args.start_ip,args.end_ip)
+    print('before rows=',df.shape[0],'\nafter rows=',new_df.shape[0])
 
 #export to file
-new_df.to_csv('vulnerabilities_en.csv', index = False)
-new_dfCt.to_csv('vulnerabilities_ct.csv', index = False)
+    new_df.to_csv(args.file_write, index = False)
+    print('success!!')
 
 
 #df.to_csv('/AfterRemoveIP/Q3/CHT/M3_part15/2/CSV/en/vulnerabilities.csv', index = False)
